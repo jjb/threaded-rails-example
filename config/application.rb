6611ffdd02eba46784ab8a47727bbe816daaae6a
John Joseph Bachir <j@jjb.cc>
2011-11-16 01:43:58 -0500

    thin and threads!
    Conflicts:
    
    	Gemfile

--- config/application.rb
+++ config/application.rb
@@ -14,6 +14,8 @@ end
 
 module HerokuThreading
   class Application < Rails::Application
+    config.threadsafe!
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,7 @@ HerokuThreading::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.threadsafe!
+
 end
